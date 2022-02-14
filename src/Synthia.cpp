3a9d27fc79352f00c5a9f8e23b20ac6649747614
hrvojekolar <hrvojekolar1998@gmail.com>
2022-02-14 11:08:32 +0100

    Add begin() LEDmatrix in Synthia

--- src/Synthia.cpp
+++ src/Synthia.cpp
@@ -1,1 +1,17 @@
 #include "Synthia.h"
+#include "Pins.hpp"
+#include <Devices/LEDmatrix/LEDmatrix.h>
+
+SynthiaImpl Synthia;
+LEDmatrixImpl LEDmatrix;
+
+void SynthiaImpl::begin(){
+	Serial.println(115200);
+
+	if(!LEDmatrix.begin(I2C_SDA, I2C_SCL)){
+		Serial.println("couldn't start matrix");
+		for(;;);
+	}
+
+
+}
--- src/Synthia.h
+++ src/Synthia.h
@@ -1,4 +1,18 @@
 #ifndef SYNTHIA_LIBRARY_SYNTHIA_H
 #define SYNTHIA_LIBRARY_SYNTHIA_H
 
-#endif //SYNTHIA_LIBRARY_SYNTHIA_H
+#include <Arduino.h>
+#include <CircuitOS.h>
+
+class SynthiaImpl {
+public:
+	SynthiaImpl();
+
+	void begin();
+
+private:
+
+};
+
+extern SynthiaImpl Synthia;
+#endif
